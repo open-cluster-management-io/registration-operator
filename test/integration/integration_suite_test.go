9e0927ccd56cee63a0412c62df33029d1035407b
zhujian <jiazhu@redhat.com>
2021-12-21 01:16:42 +0000

    add integration test
    Signed-off-by: zhujian <jiazhu@redhat.com>

--- test/integration/integration_suite_test.go
+++ test/integration/integration_suite_test.go
@@ -2,6 +2,7 @@ package integration
 
 import (
 	"context"
+	"fmt"
 	"path/filepath"
 	"testing"
 	"time"
@@ -9,10 +10,14 @@ import (
 	"github.com/onsi/ginkgo"
 	"github.com/onsi/gomega"
 
+	corev1 "k8s.io/api/core/v1"
 	apiextensionsclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/util/retry"
+	"k8s.io/klog/v2"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
@@ -38,14 +43,23 @@ const (
 )
 
 var testEnv *envtest.Environment
-
 var kubeClient kubernetes.Interface
-
 var apiExtensionClient apiextensionsclient.Interface
-
 var restConfig *rest.Config
 
 var operatorClient operatorclient.Interface
+
+// detachedTestEnv, detachedKubeClient, detachedAPIExtensionClient and detachedRestConfig is using in Detached mode.
+// for cluster manager, it represents the hub cluster;
+// while for klusterlet, it represents the managed cluster.
+var (
+	detachedTestEnv            *envtest.Environment
+	detachedKubeClient         kubernetes.Interface
+	detachedAPIExtensionClient apiextensionsclient.Interface
+	detachedRestConfig         *rest.Config
+)
+
+var cancel context.CancelFunc
 
 var _ = ginkgo.BeforeSuite(func(done ginkgo.Done) {
 	logf.SetLogger(zap.New(zap.WriteTo(ginkgo.GinkgoWriter), zap.UseDevMode(true)))
@@ -67,7 +81,6 @@ var _ = ginkgo.BeforeSuite(func(done ginkgo.Done) {
 		},
 	}
 	cfg, err := testEnv.Start()
-
 	gomega.Expect(err).ToNot(gomega.HaveOccurred())
 	gomega.Expect(cfg).ToNot(gomega.BeNil())
 
@@ -82,7 +95,28 @@ var _ = ginkgo.BeforeSuite(func(done ginkgo.Done) {
 
 	operatorClient, err = operatorclient.NewForConfig(cfg)
 	gomega.Expect(err).ToNot(gomega.HaveOccurred())
+	gomega.Expect(operatorClient).ToNot(gomega.BeNil())
+
+	// start a local kube-apiserver as the hub/managed cluster for Detached mode.
+	detachedTestEnv = &envtest.Environment{
+		ErrorIfCRDPathMissing: true,
+		CRDDirectoryPaths: []string{
+			filepath.Join(".", "deploy", "cluster-manager", "olm-catalog", "cluster-manager", "manifests"),
+			filepath.Join(".", "deploy", "klusterlet", "olm-catalog", "klusterlet", "manifests"),
+		},
+	}
+	detachedConfig, err := detachedTestEnv.Start()
+	gomega.Expect(err).ToNot(gomega.HaveOccurred())
+	gomega.Expect(cfg).ToNot(gomega.BeNil())
+
+	// prepare clients
+	detachedKubeClient, err = kubernetes.NewForConfig(detachedConfig)
+	gomega.Expect(err).ToNot(gomega.HaveOccurred())
 	gomega.Expect(kubeClient).ToNot(gomega.BeNil())
+
+	detachedAPIExtensionClient, err = apiextensionsclient.NewForConfig(detachedConfig)
+	gomega.Expect(err).ToNot(gomega.HaveOccurred())
+	gomega.Expect(apiExtensionClient).ToNot(gomega.BeNil())
 
 	// prepare a ClusterManager
 	clusterManager := &operatorapiv1.ClusterManager{
@@ -99,6 +133,12 @@ var _ = ginkgo.BeforeSuite(func(done ginkgo.Done) {
 	gomega.Expect(err).ToNot(gomega.HaveOccurred())
 
 	restConfig = cfg
+	detachedRestConfig = detachedConfig
+
+	ctx, c := context.WithCancel(context.TODO())
+	cancel = c
+
+	go ServiceAccountCtl(ctx)
 
 	close(done)
 }, 60)
@@ -108,4 +148,85 @@ var _ = ginkgo.AfterSuite(func() {
 
 	err := testEnv.Stop()
 	gomega.Expect(err).ToNot(gomega.HaveOccurred())
+
+	err = detachedTestEnv.Stop()
+	gomega.Expect(err).ToNot(gomega.HaveOccurred())
+
+	cancel()
 })
+
+// ServiceAccountCtl watch service accounts and create a corresponding secret for it.
+func ServiceAccountCtl(ctx context.Context) {
+	w, err := detachedKubeClient.CoreV1().ServiceAccounts("").Watch(ctx, metav1.ListOptions{})
+	gomega.Expect(err).ToNot(gomega.HaveOccurred())
+	klog.Infof("service account controller start")
+
+	for {
+		select {
+		case event, ok := <-w.ResultChan():
+			if !ok {
+				klog.Infof("channel closed, service account controller exit")
+				return
+			}
+
+			sa, ok := event.Object.(*corev1.ServiceAccount)
+			if !ok {
+				klog.Infof("not a service account, contine")
+				continue
+			}
+
+			if len(sa.Secrets) != 0 {
+				klog.Infof("service account secret already exist")
+				continue
+			}
+
+			secret := &corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      fmt.Sprintf("%s-token-test", sa.Name),
+					Namespace: sa.Namespace,
+					Annotations: map[string]string{
+						"kubernetes.io/service-account.name": sa.Name,
+					},
+				},
+				Data: map[string][]byte{
+					"token": []byte("test-token"),
+				},
+				Type: corev1.SecretTypeServiceAccountToken,
+			}
+
+			_, err = detachedKubeClient.CoreV1().Secrets(sa.Namespace).Create(ctx, secret, metav1.CreateOptions{})
+			if errors.IsAlreadyExists(err) {
+				klog.Infof("secret %s/%s already exist", secret.Namespace, secret.Name)
+			} else {
+				gomega.Expect(err).ToNot(gomega.HaveOccurred())
+			}
+
+			_, err = detachedKubeClient.CoreV1().Secrets(sa.Namespace).Get(ctx, secret.Name, metav1.GetOptions{})
+			gomega.Expect(err).ToNot(gomega.HaveOccurred())
+
+			err := retry.OnError(retry.DefaultBackoff,
+				func(e error) bool {
+					return true
+				},
+				func() error {
+					serviceAccount, err := detachedKubeClient.CoreV1().ServiceAccounts(sa.Namespace).Get(ctx, sa.Name, metav1.GetOptions{})
+					if err != nil {
+						return err
+					}
+					serviceAccount.Secrets = []corev1.ObjectReference{
+						{
+							Namespace: secret.Namespace,
+							Name:      secret.Name,
+						},
+					}
+					_, err = detachedKubeClient.CoreV1().ServiceAccounts(serviceAccount.Namespace).Update(ctx, serviceAccount, metav1.UpdateOptions{})
+					return err
+				})
+			gomega.Expect(err).ToNot(gomega.HaveOccurred())
+		case <-ctx.Done():
+			klog.Infof("service account controller exit")
+			return
+		}
+	}
+
+}
